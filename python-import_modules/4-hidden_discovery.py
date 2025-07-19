98a4dceab0da225583a63654a7d41c4d81721330
sarunh10 <sarunh10@gmail.com>
2025-07-19 20:02:35 +0000

    task 4 Who are you?

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+import hidden_4
+
 if __name__ == "__main__":
-    import hidden_4
-    for names in dir(hidden_4):
-        if names[0] != '_':
-            print("{}".format(names))
+    names = dir(hidden_4)
+    for name in sorted(names):
+        if not name.startswith("__"):
+            print(name)
