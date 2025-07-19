deecedeb8e97b8d9885392daae919b01f613d515
sarunh10 <sarunh10@gmail.com>
2025-07-19 00:19:37 +0000

    task Create a new sentence

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-word1 = "object-oriented"
-word2 = "programming"
-word3 = "with"
-word4 = "Python"
-
-print(word1 + " " + word2 + " " + word3 + " " + word4)
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str[39:55] + str[55:56] + str[56:67] + str[67:68] + str[68:72] + str[72:73] + str[73:79])
 
