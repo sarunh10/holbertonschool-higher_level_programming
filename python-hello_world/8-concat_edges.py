7b297afff70dd81ce54d52be712ae73f0f6d696c
sarunh10 <sarunh10@gmail.com>
2025-07-19 01:05:29 +0000

    Create a new sentence

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Holberton"
-str2 = "School"
-concat = str1 + " " + str2
-print(f"Welcome to {concat}!")
+str = ("Python is an interpreted, interactive, object-oriented programming"
+       " language that combines remarkable power with very clear syntax")
+str = "object-oriented programming with Python"
+print(str)
