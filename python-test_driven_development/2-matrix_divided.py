7687258d25bce8736b5a25378029a46b6d7e90c9
sarunh10 <sarunh10@gmail.com>
2025-07-26 19:21:44 +0000

    task 2-matrix_divided.py

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,44 +1,28 @@
 #!/usr/bin/python3
 """
-This module defines the matrix_divided function
-
+This module defines a function that divides all elements of a matrix.
 """
+
+
 def matrix_divided(matrix, div):
     """
-    Function that divide all elemtens from a matrix
-
-    Args:
-        matrix (list): Matrix.
-        div (int): divisor.
-    Returns:
-        list: new Matrix divided
+    Divides all elements of a matrix by a given number,
+    rounded to 2 decimal places.
     """
-
-    new_matrix = []
-
-    err_msg = "matrix must be a matrix (list of lists) of integers/floats"
-
-    if type(div) not in [int, float]:
+    if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
-
-    if div is 0:
+    if div == 0 or div != div:  # div != div covers float('nan')
         raise ZeroDivisionError("division by zero")
 
-    if type(matrix) is not list or matrix == []:
-        raise TypeError(err_msg)
-    else:
-        longitude = len(matrix[0])
+    if (not isinstance(matrix, list) or
+        not all(isinstance(row, list) for row in matrix) or
+        not all(isinstance(num, (int, float))
+                for row in matrix for num in row)):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
 
-    for f_matrix in matrix:
-        if any(type(x) not in [int, float] for x in f_matrix):
-            raise TypeError(err_msg)
+    row_length = len(matrix[0])
+    if not all(len(row) == row_length for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
 
-        if type(f_matrix) is not list or f_matrix == []:
-            raise TypeError(err_msg)
-
-        if len(f_matrix) is not longitude:
-            raise TypeError("Each row of the matrix must have the same size")
-
-        new_matrix.append(list(map(lambda x: round(x / div, 2), f_matrix)))
-
-    return new_matrix
+    return [[round(num / div, 2) for num in row] for row in matrix]
