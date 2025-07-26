057a9b84861fe61c0c6fbc657896395dec2cf652
sarunh10 <sarunh10@gmail.com>
2025-07-26 01:04:28 +0000

    11. Delete at

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -3,7 +3,4 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
     del my_list[idx]
-    return my_list
-lst = [1, 2, 3, 4, 5]
-print(delete_at(lst, 3))
-print(lst)              
+    return my_list 
