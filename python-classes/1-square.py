686f2af0cf2f8bf531b63cc4b4145f67f9df1442
sarunh10 <sarunh10@gmail.com>
2025-08-01 17:37:06 +0000

    task Square with size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""This module defines a Square class."""
+
+
 class Square:
-    """Class that defines a square"""
+    """Represents a square."""
     def __init__(self, size):
         self.__size = size
