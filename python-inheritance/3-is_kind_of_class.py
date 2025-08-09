c6b721d815d716baeee5b2faf4608cf35e607011
sarunh10 <sarunh10@gmail.com>
2025-08-09 12:42:53 +0000

    task Same class or inherit from

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that defines the MyList class."""
+
+
+def is_kind_of_class(obj, a_class):
+    """Return True if the object is an instance from specified class."""
+    return isinstance(obj, a_class)
