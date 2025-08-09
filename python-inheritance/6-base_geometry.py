cb4c2fffef2812a8ea35c9139d36ad585bded615
sarunh10 <sarunh10@gmail.com>
2025-08-09 12:49:16 +0000

    task Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module defines an empty class."""
+
+
+class BaseGeometry:
+    """Represents an empty class."""
+
+    def area(self):
+        raise Exception("area() is not implemented")
