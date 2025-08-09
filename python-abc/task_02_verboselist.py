46693c4766ddc5e1a92fa378841e21a28c3c2a67
sarunh10 <sarunh10@gmail.com>
2025-08-09 19:51:53 +0000

    task 2

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+
+class VerboseList(list):
+
+    def append(self, item):
+        super().append(item)
+        print(f"Added [{item}] to the list")
+
+    def extend (self, item):
+        super().extend (item)
+        print(f"Extended the list with [{item}] items..")
+
+    def remove(self, item):
+        super().remove(item)
+        print(f"Removed [{item}] from the list.")
+        
+    
+    def pop(self, index=-1):
+        item = super().pop(index)
+        print(f"Popped [{item}] from the list")
+        return item
