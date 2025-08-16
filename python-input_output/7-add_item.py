0a81845d9c4933983282de2b61534e216f189bf6
sarunh10 <sarunh10@gmail.com>
2025-08-16 19:39:08 +0000

    task Load, add, save

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+"""Add all command-line arguments to a list and save it to add_item.json."""
+
+
+import sys
+import os
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+
+filename = "add_item.json"
+
+if os.path.exists(filename):
+    items = load_from_json_file(filename)
+else:
+    items = []
+
+items.extend(sys.argv[1:])
+
+save_to_json_file(items, filename)
