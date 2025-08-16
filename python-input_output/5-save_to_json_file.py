f87166958f31a728a3b87c91bf3b95f80a8e5a0a
sarunh10 <sarunh10@gmail.com>
2025-08-16 19:32:49 +0000

    task From JSON string to Object

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 """
 Module that provides a function to save a Python
 object to a file in JSON format.
@@ -9,5 +11,5 @@ def save_to_json_file(my_obj, filename):
     """
     Write a Python object to a text file using its JSON representation.
     """
-    with open(filename, "w", encoding="utf-8") as c:
+    with open(filename, "w", enicoding="utf-8") as c:
         return j.dump(my_obj, c)
