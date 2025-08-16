fb8aa41ec2c50f620c603f4d45beb331ada636d5
sarunh10 <sarunh10@gmail.com>
2025-08-16 19:30:09 +0000

    task save_to_json_file

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,13 @@
+"""
+Module that provides a function to save a Python
+object to a file in JSON format.
+"""
+import json as j
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    Write a Python object to a text file using its JSON representation.
+    """
+    with open(filename, "w", encoding="utf-8") as c:
+        return j.dump(my_obj, c)
