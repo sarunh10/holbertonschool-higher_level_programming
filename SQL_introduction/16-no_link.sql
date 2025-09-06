72209070dc47cfaa1b52c2d21055507d9ac3e838
sarunh10 <sarunh10@gmail.com>
2025-09-06 17:59:27 +0000

    task Say my name

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
-- lists all records of the table second_table --
+-- lists all records of the table second_table --
 SELECT score, name FROM second_table 
 WHERE name IS NOT null
 ORDER BY score DESC;
